2ce07ea2bcea5ac93479c2ea02dd155924ca4039
TylerN93 <107442594+TylerN93@users.noreply.github.com>
2022-08-02 23:27:05 -0500

    finished project

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ArraysAndLists
 {
     class Program
@@ -27,6 +27,35 @@ namespace ArraysAndLists
              *
              * Try to be creative in your display
              */
+
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            var evens = new List<int>();
+
+            var odds = new List<int>();
+
+            foreach(var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+            }
+            
+            foreach (var num in evens)
+            {
+                Console.WriteLine($"{num} is even");
+            }
+
+            foreach (var num in odds)
+            {
+                Console.WriteLine($"{num} is odd");
+            }
+
         }
     }
 }
